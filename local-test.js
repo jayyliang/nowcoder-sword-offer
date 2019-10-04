9d8ab3433cd1117e6c24c713824628d80421c023
piggerCoder <1702518175@qq.com>
2019-10-04 14:18:26 +0800

    45-55

--- local-test.js
+++ local-test.js
@@ -1,19 +1,7 @@
 /**
  *用于本地写题
  */
-function FindNumbersWithSum(arr, sum) {
-    // write code here
-    let left = 0,
-        right = arr.length - 1
-    while (left < right) {
-        if (arr[left] + arr[right] > sum) {
-            right--
-        } else if (arr[left] + arr[right] < sum) {
-            left++
-        } else {
-            return [arr[left], arr[right]]
-        }
-    }
-    return []
+function isNumeric(s) {
+    return !isNaN(s)
 }
-console.log(FindNumbersWithSum([1, 2, 9, 11], 12));
+console.log(isNumeric('12e'));
