ac12d5f986a08a33ba8c6b5024a875341fec9a56
piggerCoder <1702518175@qq.com>
2019-10-04 14:46:52 +0800

    增加树的遍历

--- local-test.js
+++ local-test.js
@@ -1,7 +1,17 @@
 /**
  *用于本地写题
  */
-function isNumeric(s) {
-    return !isNaN(s)
+function maxInWindows(num, size) {
+    // write code here
+    let arr = []
+    for (var i = 0; i <= num.length - size; i++) {
+        let j = i + size
+        let temp = num.slice(i, j)
+        console.log(temp)
+        arr.push(Math.max(...temp))
+    }
+
+
+    return arr
 }
-console.log(isNumeric('12e'));
+console.log(maxInWindows([2, 3, 4, 2, 6, 2, 5, 1], 3));
