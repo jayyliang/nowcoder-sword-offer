bff136e5d86cd5f72adc0a0a43259632b59a0e4f
piggerCoder <1702518175@qq.com>
2019-10-05 12:44:50 +0800

    56-63

--- local-test.js
+++ local-test.js
@@ -1,17 +1,11 @@
 /**
  *用于本地写题
  */
-function maxInWindows(num, size) {
+function func(arr) {
     // write code here
-    let arr = []
-    for (var i = 0; i <= num.length - size; i++) {
-        let j = i + size
-        let temp = num.slice(i, j)
-        console.log(temp)
-        arr.push(Math.max(...temp))
-    }
 
-
-    return arr
 }
-console.log(maxInWindows([2, 3, 4, 2, 6, 2, 5, 1], 3));
+function compose() {
+    
+}
+console.log(func([[1, 2, 3], ['a', 'b', 'c'], ['!', '@', '#']]));
